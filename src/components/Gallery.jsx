0c37288ceccc1a3de02d57681a9b162b4b906c8e
Wallysson <wlc.couto@gmail.com>
2022-07-05 11:28:13 -0300

    importando gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,12 +1,16 @@
 import styles from './Gallery.module.css'
 
+import galleryUm from '../assets/gallery-1.png'
+import galleryDois from '../assets/gallery-2.png'
+import galleryTres from '../assets/gallery-3.png'
+
 export function Gallery() {
   return (
     <section className={styles.gallery}>
       <div className={styles.galleryBoxImg}>
-        <img src="src/assets/gallery-1.png" alt="Mulher pintando o quadro" />
-        <img src="src/assets/gallery-2.png" alt="Rosto de mulher no quadro" />
-        <img src="src/assets/gallery-3.png" alt="Pintura colorida" />
+        <img src={galleryUm} alt="Mulher pintando o quadro" />
+        <img src={galleryDois} alt="Rosto de mulher no quadro" />
+        <img src={galleryTres} alt="Pintura colorida" />
       </div>
       
       <div className={styles.content}>
