8dfb341ae763582f3430230809054fc5a2bd9f02
Wallysson <wlc.couto@gmail.com>
2022-07-05 11:30:57 -0300

    finish artists

--- src/components/Artists.jsx
+++ src/components/Artists.jsx
@@ -1,5 +1,10 @@
 import styles from './Artists.module.css'
 
+import artistUm from '../assets/artist-1.png'
+import artistDois from '../assets/artist-2.png'
+import artistTres from '../assets/artist-3.png'
+
+
 export function Artists() {
   return (
     <section className={styles.artists}>
@@ -11,21 +16,21 @@ export function Artists() {
             <strong>Sabine Lowe</strong>
             <span>Design e Fotógrafa</span>
           </div>
-          <img src="src/assets/artist-1.png" alt="Sabine Lowe" />
+          <img src={artistUm} alt="Sabine Lowe" />
         </div>
         <div className={styles.artistsContainerText}>
           <div className={styles.artistsText}>
             <strong>Sabine Lowe</strong>
             <span>Design e Fotógrafa</span>
           </div>
-          <img src="src/assets/artist-2.png" alt="Eleanor Pena" />
+          <img src={artistDois} alt="Eleanor Pena" />
         </div>
         <div className={styles.artistsContainerText}>
           <div className={styles.artistsText}>
             <strong>Sabine Lowe</strong>
             <span>Design e Fotógrafa</span>
           </div>
-          <img src="src/assets/artist-3.png" alt="Leslie Alexander" />
+          <img src={artistTres} alt="Leslie Alexander" />
         </div>
       </div>
     </section>
